21927bdd46e4a621006a3cb7837e73a342204c0a
GhanshyamJangir <ghanshyamjangir334@gmail.com>
2026-02-20 16:18:38 +0530

    Fix base path for Render and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pg-customer-app/',
+  base: process.env.PUBLIC_BASE_PATH || '/',
 })
